6e4d3a30344008df76820710991196b5727c9981
CeresBarros <ceres.barros@gov.bc.ca>
2024-01-31 14:54:07 -0800

    make sure data.table is up-to-date

--- _common.R
+++ _common.R
@@ -1,5 +1,10 @@
 options(repos = "https://cloud.r-project.org/")
 
+
+
 if (!"Require" %in% installed.packages()) install.packages("Require")
 
 Require::setLibPaths("packages", standAlone = TRUE)
+
+## make sure data.table is up-to-date
+install.packages("data.table")
